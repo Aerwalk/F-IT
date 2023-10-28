f2f6769d1690a95691ff7407a02048957801af89
Aerwalk <145575597+Aerwalk@users.noreply.github.com>
2023-10-29 01:33:29 +0400

    Update task_2.py

--- Лабораторная работа 3/task_2.py
+++ Лабораторная работа 3/task_2.py
@@ -1,5 +1,5 @@
 
-def find_common_participants(participants1, participants2, separator=","):
+def find_common_participants(participants1, participants2, separator=','):
     participants_list1 = participants1.split(separator)
     participants_list2 = participants2.split(separator)
     common_participants = list(set(participants_list1).intersection(participants_list2))
