cc3d18d747bdc51fe662cd89e7d007f5f8662a26
Aerwalk <145575597+Aerwalk@users.noreply.github.com>
2023-10-29 01:32:31 +0400

    Update task_2.py

--- Лабораторная работа 3/task_2.py
+++ Лабораторная работа 3/task_2.py
@@ -1,8 +1,7 @@
 
-def find_common_participants(participants1, participants2, separator=','):
+def find_common_participants(participants1, participants2, separator=","):
     participants_list1 = participants1.split(separator)
     participants_list2 = participants2.split(separator)
-
     common_participants = list(set(participants_list1).intersection(participants_list2))
     common_participants.sort()
     return common_participants
@@ -10,6 +9,6 @@ def find_common_participants(participants1, participants2, separator=','):
 
 participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
-print("Общие участники:", find_common_participants(participants_second_group, participants_second_group,"|"))
 
+print("Общиие участники:", find_common_participants(participants_first_group, participants_second_group, "|"))
 
